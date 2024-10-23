5569be510a2cdf880155818eb6609010a4bea82c
jhowrf <jhowrf@gmail.com>
2024-10-23 18:53:56 -0300

    feat: create notification class

---
+++ src/domain/@shared/notification/notification.ts
@@ -0,0 +1,22 @@
+export type NotificationError = {
+    context: string
+    message: string
+}
+
+export default class Notification {
+    private errors: NotificationError[] = []
+
+    addError(error: any) {
+        this.errors.push(error)
+    }
+
+    messages(context?: string): string {
+        let messages = ""
+        this.errors.forEach((error) => {
+            if (context === undefined || error.context === context) {
+                messages += `${error.context}: ${error.message},`
+            }
+        })
+        return messages
+    }
+}
